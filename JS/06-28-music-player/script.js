6b6f4eb03d12c0d631f7a8635c7f5868901832f2
Moni97sha-S <129970757+Moni97sha-S@users.noreply.github.com>
2023-07-03 21:56:49 +0530

    June dumps

--- JS/06-28-music-player/script.js
+++ JS/06-28-music-player/script.js
@@ -7,55 +7,185 @@ let bgc = [
 ]
 
 const playUList = document.getElementById("playlist");
-let playlist = document.getElementsByTagName("li");
-let audioPlayer = document.getElementById("audioPlayer");
-let nextButton = document.getElementById("nextBtn");
-let prevButton = document.getElementById("prevBtn");
-let playButton = document.getElementById("playBtn");
-let pauseButton = document.getElementById("pauseBtn");
-let favButton = document.getElementById("popupBtn");
+const playlist = document.getElementsByTagName("li");
+const audioPlayer = document.getElementById("audioPlayer");
+const nextButton = document.getElementById("nextBtn");
+const prevButton = document.getElementById("prevBtn");
+const playButton = document.getElementById("playBtn");
+const pauseButton = document.getElementById("pauseBtn");
+const popupButton = document.getElementById("popupBtn");
+const favButton = document.querySelectorAll(".fav");
+const btn = document.getElementsByClassName('btn');
+//* HTMLCollection list of ctrl btns
+// console.log("Audio Control Btns -->", btn);
 
+for(let i=0; i < btn.length; i++){
+    // btn[i].style.fontFamily = 'Times New Roman', 'Times', 'serif';
+    btn[i].style.color = "White";
+    fontsize = 15;
+    btn[i].style.fontSize = fontsize +"px";
+    btn[i].style.textTransform = "capitalize";
+    btn[i].style.letterSpacing = "2px";
+}
+//let icon = document.getElementsByTagName("i"); // item(i) due to HTML Collection of this tag elements
+const pauseIconClassName = 'fa-pause';
+const playIconClassName = 'fa-play';
 
 //! Loop for onClick of songName
-//? Run a loop on every list and attach a click event
-//* playlist - Collection of songs which is HTML Collection() here
-//? On every click my currentSong should be updated 
-//* playlist.indexOf(this -> currentSong) in this playlist is not pure array 
-//? Every element/method like indexOf() of array is not in the HTML Collection() so we use Array.Prototype
-//? Every element/method like length of array is in the HTML Collection()
 let currentSong = 0;
-for(let i=0; i<playlist.length; i++){
+for(let i=0; i < playlist.length; i++){
     playlist[i].addEventListener("click",function(){
-        currentSong = Array.prototype.indexOf.call(playlist, this) //*playlist.indexOf(this)
+        currentSong = Array.prototype.indexOf.call(playlist, this); //*playlist.indexOf(this)
         playSong(currentSong);
         document.body.style.backgroundImage = `url(${bgc[currentSong]})`;
-        // console.log("On click of a song from list got clicked one's list tag -->", playlist[i]);
     });
 }
 
-//! Function to play Song()
+//! Function to play Current Song()
 function playSong(currentSong){
     let dataSrc = playlist[currentSong].getAttribute("data-src");
     audioPlayer.src = dataSrc;
-    audioPlayer.play();
     setActiveSong(currentSong);
-    // console.log("Audio Tag -->", audioPlayer);
-}
-
+    let playPromise = audioPlayer.play();
+    // ! -> not operator which prohibits from autoplay while clicking on song from list 
+    if (playPromise !== undefined) {
+        playPromise.then(_ => {
+          // Automatic playback started!
+          // Show playing UI.
+          // We can now safely pause video...
+          playPause();
+        })
+        .catch(error => {
+          // Auto-play was prevented
+          // Show paused UI.
+        });
+      }
+    
+    }
+    
+//! Function to play Next song
+/*
+* 1. Increase currentSong by 1
+* 2. Call playSong function
+* 3. Change different background images for each song dynamically
+*/
 nextButton.addEventListener("click", function(){
     currentSong = currentSong + 1;
     playSong(currentSong);
     document.body.style.backgroundImage = `url(${bgc[currentSong]})`;
 })
 
+
+//! Function to play Previous song
 prevButton.addEventListener("click", function(){
     currentSong = currentSong - 1;
     playSong(currentSong);
     document.body.style.backgroundImage = `url(${bgc[currentSong]})`;
 })
 
-function setActiveSong(currentSong){
-    for(let i=0; i<playlist.length; i++){
-        playlist[i].classList.add('active');
+playButton.addEventListener("click", function(e){
+    // const btnEl = e.currentTarget;
+    // console.log(btnEl)
+    playPause();
+})
+
+
+//! Function for handling playPause feature
+function playPause(){
+    // check if play button class is present on our button
+    const isPlayIcon = playButton.classList.contains(playIconClassName);
+
+    if(audioPlayer.paused){
+        audioPlayer.play();
+        if (isPlayIcon){
+            console.log(pauseIconClassName)
+            playButton.classList.remove(playIconClassName);            
+            playButton.classList.add(pauseIconClassName);            
+        } 
+        playButton.innerText = " Pause";   
+    }else{
+        audioPlayer.pause();
+        if (!isPlayIcon){
+           console.log(playIconClassName)           
+           playButton.classList.remove(pauseIconClassName); 
+           playButton.classList.add(playIconClassName);   
+        } 
+        playButton.innerText = " Play";
+    }
+        // console.log(icon.item(1));
+        // icon.item(1).classList.toggle('fa-pause');
+    }
+    
+    //! Add active/current song with orange dot
+    function setActiveSong(currentSong){
+        for(let i = 0; i < playlist.length; i++){
+        if(i == currentSong){
+            playlist[i].classList.add('active');
+        } else {
+            playlist[i].classList.remove('active');
+        }
     }
 }
+
+//! Add Songs to your fav list by storing at Local Storage
+//? On click of favButton, store in LocalStorage
+//! ON click of fav btn Show popups of list of favourite songs
+//! Handling Popup for Fav Song & add to Local Storage
+// querySelectorAll() contains nodeList
+// console.log("Show Favt Song List Btns -->", favButton);
+favButton.forEach(function (button) {
+    button.addEventListener("click", function(){
+        this.innerText = "❤️";
+        // console.log(button);
+        // try to store song ids & names of liked btn by fetching song via btn id
+        let songIdName = document.getElementById(`s${button.id}`);
+        // console.log(songIdName.innerText.trim()); 
+        // window.onbeforeunload = function() {  
+        localStorage.setItem(songIdName.id, songIdName.innerText.trim());
+        
+    });
+});
+
+//! GetItem Song from LocalStorage & add to popup-Content div tag
+const popupContent = document.getElementById("popup-content");
+const popupOverlay = document.getElementById("popup-overlay");
+
+popupButton.addEventListener("click", function(){
+    popupOverlay.style.display = "flex";  
+    // console.log(Object.keys(localStorage))
+    // Object.keys will not fetch in correct order will return in any sequence
+    let keys = Object.keys(localStorage);
+    // console.log(keys);
+    //? Add to DOM on every click event
+    keys.forEach((key) => {
+        // console.log(typeof localStorage[key]);
+        // console.log(`${key}: ${localStorage[key]}`);
+        // AddToCart
+        // https://stackoverflow.com/questions/59424119/how-to-remove-or-avoid-duplicate-values-in-localstorage
+        // let arr = JSON.parse(window.localStorage.getItem("itemsLine")) || [];
+        // let value = localStorage[key];
+        // if(arr.indexOf(value) == -1){
+            //     arr.push(value);
+            // window.localStorage.setItem("itemsLine", JSON.stringify(arr));
+            let p = document.createElement("p");
+            console.log(p.innerText !== localStorage[key])
+            if(localStorage.getItem(key) !== p.innerText){
+                // for(let i=0; i<localStorage.length;i++){
+                    let songName = '';
+                    songName = localStorage.getItem(key);
+                    // console.log(songName)
+                    p.innerText = songName;
+                    console.log(p.innerText)
+                    // console.log(typeof p.innerText);
+                    popupContent.insertBefore(p, popupClose);
+            }  
+    });
+});
+
+//! On click of Popup closeBtn show popup closes
+popupClose = document.getElementById("popup-close");
+popupClose.addEventListener("click", function(){
+    popupOverlay.style.display = "none";   
+});
+
+
---
+++ JS/Final Project JS Practice Codes/My Final Proj/script.js
@@ -0,0 +1,4 @@
+/* <a href="path/to/shopping/cart" aria-label="View 3 items in your shopping cart">
+  <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+</a>
+*/
