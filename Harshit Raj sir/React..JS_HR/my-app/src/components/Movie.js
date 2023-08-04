fadc3c795f185afae0d1d3b788e91cc6638a2275
Moni97sha-S <129970757+Moni97sha-S@users.noreply.github.com>
2023-08-04 23:12:28 +0530

    updations

---
+++ Harshit Raj sir/React..JS_HR/my-app/src/components/Movie.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Movie({ element }) {
+  return element;
+}
+
+
+
+export default Movie
--- Harshit Raj sir/React..JS_HR/my-app/src/components/User.js
+++ Harshit Raj sir/React..JS_HR/my-app/src/components/User.js
@@ -1,30 +1,24 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 
-function User() {
-// useParams Hook
-//   const params = useParams();
-//   console.log(params);
-//   console.log(params.userId);
-// [OR]
-//   const { userId } = useParams();
-//   console.log({ userId });
-//   console.log({ userId }.userId);
-  const { userId, userName, userCountry } = useParams();
-  console.log(useParams()); //* Object
-  console.log({ userId, userName, userCountry }); //* Same Object as above
-  console.log({ userId }.userId);
-  console.log({ userName }.userName);
-
-  return (
-    <>
-     <h1>User Page</h1>
-     {/* <h2>User Id is {params.userId}</h2> */}
-     <h2>User Id is {userId}</h2>
-     <h2>User Name is {userName}</h2>
-     <h2>User Country is {userCountry}</h2>
-    </>
-  );
+function User(props) {
+    return (
+        <>
+          <h1>
+            {props.name} {props.salary + 200} {props.country} 
+          </h1>
+        </>
+    );
 }
+
+//Type checking
+User.propTypes = {
+    name: PropTypes.string.isRequired,
+    salary: PropTypes.number.isRequired,
+    country: PropTypes.string.isRequired,
+    info: PropTypes.array.isRequired,
+
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    gender: PropTypes.oneOf("male", "female").isRequired,
+};
 
 export default User;
